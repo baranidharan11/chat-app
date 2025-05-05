e9ee08fdc16b871936927274a262c7adbb31bc62
baranidharan11 <baraniskt11@gmail.com>
2025-05-06 00:47:27 +0530

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,25 @@
-import { applyMiddleware, createStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
-import persistStore from "redux-persist/es/persistStore";
+import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import { persistedReducer } from "./rootReducer";
+import persistStore from "redux-persist/es/persistStore";
 import rootSaga from "./rootSaga";
-
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  persistedReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
-);
-sagaMiddleware.run(rootSaga);
-export const persistor = persistStore(store);
+// Configure store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: false,
+      serializableCheck: false,
+    }).concat(sagaMiddleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
 
+
+sagaMiddleware.run(rootSaga);
+
+
+export const persistor = persistStore(store);
 export default store;
